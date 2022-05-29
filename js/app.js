0a24ccfcae4e5709fed0823195bc9c5483df41e7
Jezeel Acosta <jezeel_am@hotmail.com>
2022-05-28 20:22:14 -0600

    Bug fix de palabra repetida, boton para jugar de nuevo

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ const pantalla = document.querySelector('canvas');
 const pincel = pantalla.getContext('2d');
 const letrasMal = document.querySelector('.letrasMalas');
 const letrasPresionadas = document.querySelector('.presionadas');
+let borrarCanvas = document.querySelector("canvas");
 
 pincel.strokeStyle = "#000000"; 
 pincel.lineWidth = 4;
@@ -16,31 +17,28 @@ let buenasLetras = [];
 let presionadas = [];
 let p = [];
 let ganaste;
-let aleatorio = Math.round(Math.random()*10);
-let palabraMisteriosa = adivinar[aleatorio];
-let intentos = 0;
+let aleatorio = Math.round(Math.random()* adivinar.length);
+let palabraMisteriosa = adivinar[aleatorio]; 
     
 botonJuego.addEventListener("click", function(e) {
     e.preventDefault();
-
-    if (botonJuego.textContent == "Nuevo Juego") { 
-        location.reload();
-    }
     if(botonJuego.textContent == "Iniciar juego") {
         canvasOn(); 
         crearCanvas(); 
         presionadasLetras();
+        formarPalabra();
+        escucharLetras();  
     }
-    for(let i = 0; i < palabraMisteriosa.length; i++) {
-        crearTd();
-        crearClase();
+    if (botonJuego.textContent == "Nuevo Juego") { 
+        reiniciarJuego();
+        presionadasLetras();
+        formarPalabra(); 
     }
-    escucharLetras();   
+ 
 });
 botonJuego2.addEventListener("click", function(e) {
     e.preventDefault();
     if(botonJuego2.textContent == "Cancelar") {
-        canvasOff();
         location.reload();
     }   
 });
@@ -48,38 +46,10 @@ function escucharLetras() { document.addEventListener("keypress", function(event
     let tecla = event.key;
     let permitidos = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
     let noPermitidas = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']; 
-
-    for (let i = 0; i < permitidos.length; i++) {           
+    for (let i = 0; i < permitidos.length; i++) {         
         const letra = permitidos[i]; 
-
-        if(tecla == letra) {
-            if (presionadas.includes(tecla)) {
-                swal("", "Ya presionaste esta letra!", "warning")
-            } else {
-                presionadas.push(tecla);
-            }
-            if (palabraMisteriosa.search(letra) == -1) {
-                if (!malasLetras.includes(letra)) {
-                    malasLetras.push(tecla);  
-                }
-            }
-            if(malasLetras.length == 10) {
-                swal("Perdiste!", "La palabra era " + palabraMisteriosa + " " , "error");
-            }  
-            letrasPresionadas.textContent = presionadas.join(" ");
-            crearAhoracado();
-            crearLetras(tecla, letra);
-            if (p.length === buenasLetras.length) {
-                ganasteAlerta();
-            }
-        }
-            for (let i = 0; i < noPermitidas.length; i++) {
-            const letrasMinuculas = noPermitidas[i];
-            if(tecla == letrasMinuculas)  {
-                alertaError();              
-                break;
-            }
-        }      
+        juego(tecla, letra, noPermitidas);
+        
     }       
 });
 }
@@ -91,83 +61,72 @@ function canvasOn() {
     botonJuego2.textContent = "Cancelar"
     botonJuego.textContent = "Nuevo Juego"   
 }
-function canvasOff() {
-    cambiarBotones.classList.remove("canvasOn");
-    botonJuego.classList.remove("boton");
-    botonJuego2.classList.remove("boton");
-    botonJuego2.textContent = "Agregar nueva palabra"
-    botonJuego.textContent = "Iniciar juego"
-    removerCanvas();
-}
 function crearCanvas() {
     pantalla.classList.remove('removerCanvas');
-
 }
 function removerCanvas() {
-    let borrarCanvas = document.querySelector("canvas");
     if (borrarCanvas > "") {
         borrarCanvas.classList.add('removerCanvas');
     }
 }
 function alertaError() {
-    swal("Ups!", "Solo letras y que sean mayúsculas, por favor", "error");
+    swal("Ups!", "Solo letras y que sean mayúsculas, por favor", "error", {button: false});
 }
 function crearTd() {
     let tdletra = document.createElement("td");
     letrasMal.appendChild(tdletra);
 }   
 function crearAhoracado() {
-    if(malasLetras.length == 1) {
+    if(malasLetras.length === 1) {
         pincel.moveTo(90, 125); // Base de la horca
         pincel.lineTo(140, 125);
         pincel.stroke();
     }
-    if(malasLetras.length == 2){
+    if(malasLetras.length === 2){
         pincel.moveTo(115, 125); // Altura de la Horca
         pincel.lineTo(115, 21);
         pincel.stroke();
     }
-    if(malasLetras.length == 3) {
+    if(malasLetras.length === 3) {
         pincel.moveTo(113, 20); // Base para la cuerda
         pincel.lineTo(160, 20);
         pincel.stroke();
     }
-    if(malasLetras.length == 4){
+    if(malasLetras.length === 4){
         pincel.moveTo(158, 20); // Base para el cuello 
         pincel.lineTo(158, 50);
         pincel.stroke();
     }
-    if(malasLetras.length == 5){
+    if(malasLetras.length === 5){
         pincel.beginPath(); // Cabeza 
         pincel.arc(158, 61, 13, 0, 2 * Math.PI, false);
         pincel.stroke();
     }
-    if(malasLetras.length == 6) {
+    if(malasLetras.length === 6) {
         pincel.moveTo(158, 76); // Cuerpo
         pincel.lineTo(158, 100);
         pincel.stroke();
     }
-    if(malasLetras.length == 7) {
+    if(malasLetras.length === 7) {
         pincel.moveTo(158, 80); // Brazo derecha
         pincel.lineTo(168, 98);
         pincel.stroke();
     }
-    if(malasLetras.length == 8) {
+    if(malasLetras.length === 8) {
         pincel.moveTo(158, 80); // Brazo izquierdo
         pincel.lineTo(148, 98);
         pincel.stroke();
     }
-    if(malasLetras.length == 9) {
+    if(malasLetras.length === 9) {
         pincel.moveTo(158, 99) // Pierna derecha
         pincel.lineTo(168, 115);
         pincel.stroke();
     }
-    if(malasLetras.length == 10) {
+    if(malasLetras.length === 10) {
         pincel.moveTo(158, 99) // Pierna izquierda
         pincel.lineTo(148, 115);
         pincel.stroke();
     }  
-
 }
 function crearLetras(tecla) {
     let td = document.querySelectorAll("td");
@@ -210,7 +169,14 @@ function ganasteAlerta() {
     ganaste = window.setTimeout(slowAlert, 500);     
 }    
 function slowAlert() {
-  swal("Ganaste!", "", "success");
+  swal("Ganaste!", "", "success", {
+      button: "Jugar de Nuevo",
+  })
+  .then(() =>{
+        reiniciarJuego();
+        presionadasLetras();
+        formarPalabra();  
+  });
 }
 function meterLetras(letra) {
     if (!buenasLetras.includes(letra)) {
@@ -222,5 +188,67 @@ function presionadasLetras() {
         if(!p.includes(i)) {
             p.push(i);
         }
+    }
+}
+function juego(tecla, letra, noPermitidas) {
+
+    if(tecla == letra) {
+        if (!presionadas.includes(tecla)) {
+            presionadas.push(letra);
+        } else {
+             swal("", "Ya presionaste esta letra!", "warning", {button: false});
+        }
+        if (palabraMisteriosa.search(letra) == -1) {
+            if (!malasLetras.includes(letra)) {
+                malasLetras.push(tecla);  
+            }
+        }
+        if(malasLetras.length == 10) {
+            swal("Perdiste!", "La palabra era " + palabraMisteriosa + " " , "error", {
+                button: "Intentarlo de Nuevo",
+            })
+            .then(() =>{
+                  reiniciarJuego();
+                  presionadasLetras();
+                  formarPalabra();  
+            });
+        }  
+        letrasPresionadas.textContent = presionadas.join(" ");
+        crearAhoracado();
+        crearLetras(tecla, letra);
+        if (p.length === buenasLetras.length) {
+            ganasteAlerta();
+
+        } 
+    }
+        for (let i = 0; i < noPermitidas.length; i++) {
+        const letrasMinuculas = noPermitidas[i];
+        if(tecla == letrasMinuculas)  {
+            alertaError();              
+            break;
+        }
+    } 
+}
+function reinciarCanvas() {
+    const context = borrarCanvas.getContext('2d');
+    context.clearRect(0, 0, borrarCanvas.width, borrarCanvas.height);
+    context.beginPath();
+
+}
+function reiniciarJuego () { 
+    letrasMal.innerHTML = "";
+    letrasPresionadas.innerHTML = "";
+    presionadas = [];
+    malasLetras = [];   
+    buenasLetras = [];
+    p.length = 0
+    aleatorio = Math.round(Math.random()* adivinar.length);
+    palabraMisteriosa = adivinar[aleatorio];
+    reinciarCanvas();  
+}
+function formarPalabra() {
+    for(let i = 0; i < palabraMisteriosa.length; i++) {
+        crearTd();
+        crearClase();
     }
 }
